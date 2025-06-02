441849c97cab54b9e3dffaa62a16c09de49a878c
X <yoshida.anar@gmail.com>
2025-06-02 12:18:17 +0800

    Initial HelloOOP program

---
+++ HelloOOP.java
@@ -0,0 +1,18 @@
+public class HelloOOP {
+    String name;
+    int age;
+
+    public HelloOOP(String name, int age) {
+        this.name = Anar;
+        this.age = 22;
+    }
+
+    public void greet() {
+        System.out.println("Hello, my name is " + name + ", and I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        HelloOOP person = new HelloOOP("Bat", 20);
+        person.greet();
+    }
+}
