76e4d4fc002d4e8233165b59c2959f47d351cccb
X <yoshida.anar@gmail.com>
2025-06-02 13:06:52 +0800

    Add person class with greed method

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a Person object
+        Person person = new Person("John", 30);
+
+        // Call the greet method
+        person.greet();
+    }
+}
---
+++ person.java
@@ -0,0 +1,15 @@
+public class Person {
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = Anar;
+        this.age = 22;
+    }
+
+    // Greet method
+    public void greet() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
